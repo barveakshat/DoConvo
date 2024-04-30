13f84740d8ab00f7aafdc4c1b8ede81d487c1f51
Akshat Barve <barveakshat091@gmail.com>
2024-04-30 10:11:36 +0530

    scrollbar

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -40,7 +40,7 @@ const ChatComponent = ({ chatId }: Props) => {
   }, [messages]);
   return (
     <div
-      className="relative max-h-screen overflow-hidden "
+      className="relative max-h-screen overflow-scroll "
       id="message-container"
     >
       {/* header */}
